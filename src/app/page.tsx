ef4a5cadb6ad3401c770619ce9238f73ba6c9f2d
Manku27 <jjmayank98@gmail.com>
2025-07-26 22:19:56 +0530

    add home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
+import { HomeEventList } from '@/components/home/events';
+
 import styles from './page.module.css';
 
 export default function Home() {
   return (
     <main className={styles.container}>
-      <h1>my styles</h1>
+      <div className="heading1">Hey, Boss!</div>
+      <HomeEventList />
+      <section>
+        <h2>Quick Expense</h2>
+      </section>
+      <section>
+        <h2>I just worked</h2>
+      </section>
     </main>
   );
 }
