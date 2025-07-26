af08b6982481b1f34751fd8e3222cb7f50ba3dd0
Manku27 <jjmayank98@gmail.com>
2025-07-26 11:21:09 +0530

    add eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,68 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: __dirname
 });
-
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: [
+      'next/core-web-vitals',
+      'next/typescript',
+      'plugin:prettier/recommended',
+      'prettier'
+    ],
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_' }
+      ],
+      'jsx-quotes': ['error', 'prefer-double'],
+      'import/extensions': [
+        'warn',
+        {
+          js: 'never',
+          jsx: 'never',
+          json: 'never',
+          css: 'always',
+          server: 'always',
+          client: 'always'
+        }
+      ],
+      'import/order': [
+        'warn',
+        {
+          'newlines-between': 'always',
+          warnOnUnassignedImports: true,
+          groups: ['builtin', 'external', 'internal'],
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true
+          },
+          pathGroupsExcludedImportTypes: ['react']
+        }
+      ],
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'auto'
+        }
+      ]
+    },
+    settings: {
+      'import/resolver': {
+        typescript: {
+          alwaysTryTypes: true
+        }
+      }
+    }
+  }),
+  { ignores: ['**/*.generated.ts'] }
 ];
 
 export default eslintConfig;
