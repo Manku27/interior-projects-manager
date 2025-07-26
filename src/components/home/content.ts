68a2ef70a08d12eb8c63722c3bc18d6720944390
Manku27 <jjmayank98@gmail.com>
2025-07-26 22:20:09 +0530

    add home events

---
+++ src/components/home/content.ts
@@ -0,0 +1,13 @@
+export const eventList = [
+  {
+    project: 'Project A',
+    item: 'Measurement',
+    vendor: 'Vendor 1',
+    time: '2pm'
+  },
+  {
+    project: 'Project B',
+    item: 'Measurement',
+    time: '3pm'
+  }
+];
